f9176a846d06ac444a802f476cd0a30c01f18ee7
tjohn0312 <tyleej0312@gmail.com>
2023-10-22 19:13:56 -0400

    added start of skill search

---
+++ README.md
@@ -0,0 +1,10 @@
+started rudimentary search function
+
+# todo:
+------
+use regex to search for all LV's of selectedSkills
+store what lv is selected though
+
+make selctedSkills an array to search for multiple skills
+
+organise skills by level & priority
--- script.js
+++ script.js
@@ -1,50 +1,55 @@
 // thunder el and ice need to be switched in web scraper
 let out = "";
 let i = 10;
+let selectedSkills = "Weakness Exploit Lv 2"
 do {
     console.log("starting rarity "+i) 
-    fetch("./data/rarity10.json")
+    fetch("./data/rarity"+i+".json")
         .then(function(response){
             return response.json();
         })
         .then(function(products){
             let dataOut = document.querySelector("#data-output");
             for(let product of products){
-                out += `
-                    <tr>
-                        <td>${product.title}</td>
-                        <td>
-                            <table>
-                                <tr><td>${product.slots[0]}</td></tr>
-                                <tr><td>${product.slots[1]}</td></tr>
-                                <tr><td>${product.slots[2]}</td></tr>
-                            </table>
-                        </td>
-                        <td>
-                            <table>
-                                <tr><td>Defence: ${product.defences[0]}</td></tr>
-                                <tr><td>${product.defences[1]}</td></tr>
-                                <tr><td>${product.defences[2]}</td></tr>
-                            </table>
-                        </td>
-                        <td>
-                            <table>
-                                <tr><td>${product.defences[4]}</td></tr>
-                                <tr><td>${product.defences[3]}</td></tr>
-                                <tr><td>${product.defences[5]}</td></tr>
-                            </table>
-                        </td>
-                        <td>
-                            <table>
-                                <tr><td>${product.skills[0]}</td></tr>
-                                <tr><td>${product.skills[1]}</td></tr>
-                                <tr><td>${product.skills[2]}</td></tr>
-                                <tr><td>${product.skills[3]}</td></tr>
-                                <tr><td>${product.skills[4]}</td></tr>
-                            </table>
-                        </td>
-                    </tr>
-                    `
+                let armorSkills = product.skills
+                console.log(armorSkills)
+                if (armorSkills.includes(selectedSkills)) {
+                    out += `
+                        <tr>
+                            <td>${product.title}</td>
+                            <td>
+                                <table>
+                                    <tr><td>${product.slots[0]}</td></tr>
+                                    <tr><td>${product.slots[1]}</td></tr>
+                                    <tr><td>${product.slots[2]}</td></tr>
+                                </table>
+                            </td>
+                            <td>
+                                <table>
+                                    <tr><td>Defence: ${product.defences[0]}</td></tr>
+                                    <tr><td>${product.defences[1]}</td></tr>
+                                    <tr><td>${product.defences[2]}</td></tr>
+                                </table>
+                            </td>
+                            <td>
+                                <table>
+                                    <tr><td>${product.defences[4]}</td></tr>
+                                    <tr><td>${product.defences[3]}</td></tr>
+                                    <tr><td>${product.defences[5]}</td></tr>
+                                </table>
+                            </td>
+                            <td>
+                                <table>
+                                    <tr><td>${product.skills[0]}</td></tr>
+                                    <tr><td>${product.skills[1]}</td></tr>
+                                    <tr><td>${product.skills[2]}</td></tr>
+                                    <tr><td>${product.skills[3]}</td></tr>
+                                    <tr><td>${product.skills[4]}</td></tr>
+                                </table>
+                            </td>
+                        </tr>
+                        `
+                }
             }
             dataOut.innerHTML = out;
         })
